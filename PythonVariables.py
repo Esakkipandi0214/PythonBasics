72f660a7d02cf0193eb9a259f6ddded549dc7cad
Esakkipandi0214 <praveenpandian01@gmail.com>
2025-02-23 10:10:29 +0530

    python variables

---
+++ PythonVariables.py
@@ -0,0 +1,20 @@
+# Variables are containers for storing data values .
+
+"""
+1. Python  is dynamically typed , which means you don't have to specify the type of a variable .
+2. It is determined automatically based on the assigned value .
+"""
+# Python has no commands for declaring a variable .
+
+
+""" Below Example for create a vairable and print in Terminal """
+
+x = 5; # create variable "X" and assign a value 5 to it using "=" assignment operator 
+y= "Pandi" # similar to above but a string value .
+
+# to print the variables use "print" statement .
+print(x);
+print(y);
+
+# to excecute use command below in your terminal
+#  syntax ->  python PythonVariables.py
