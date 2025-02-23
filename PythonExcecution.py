bad394c35c64d983db8db0c7579091fc0dbb9e7b
Esakkipandi0214 <praveenpandian01@gmail.com>
2025-02-23 09:58:13 +0530

    Python excecution

---
+++ PythonExcecution.py
@@ -0,0 +1,17 @@
+# this # -> is used to command the text which is not read by compiler while compiling or excecuting
+
+# to print the output on termial we print funtion 
+#for example
+
+print('Hello , world');
+
+# to run this python program use syntax 
+# syntax : python PythonExcecution.py
+
+
+
+
+# ============================================== Little Info ===========================================
+
+# Python Indentation -> Python uses indentation to indicate a block code .
+# Python will give error if you skip the indentation .
